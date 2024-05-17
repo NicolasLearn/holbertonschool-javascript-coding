2acd873d140216615c5d1c84739a2463a67a57b7
nicolas <nicolas.quoirez@hotmail.fr>
2024-05-17 12:12:11 +0200

    writing script to file 0 and 1

---
+++ 0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const process = require('process');
+const filesystem = require('fs');
+
+const file = process.argv[2];
+
+filesystem.readFile(file, 'utf8', function (err, data) {
+  if (err != null) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
+});
