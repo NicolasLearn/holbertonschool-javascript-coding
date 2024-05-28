b9133570a4025d0b01d84db88eea83bb4dc17c5e
nicolas <nicolas.quoirez@hotmail.fr>
2024-05-28 15:03:20 +0200

    fix error task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,21 +1,16 @@
 // Display the welcome message
-console.log("Welcome to Holberton School, what is your name?");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Read user input from stdin
-process.stdin.on('data', (data) => {
-  // Convert input to string and trim whitespace
-  const name = data.toString().trim();
-
-  // Display the user's name
-  console.log(`Your name is: ${name}`);
-
-  // Display the closing message
-  console.log("This important software is now closing");
-  process.exit();
+// Add an event handler for 'readable' to stdin
+process.stdin.on('readable', () => {
+  // Read available data from stdin 
+  const name = process.stdin.read();
+  // If Data is not null, display the name of the user
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
-
-// Handle program exit (if user uses Ctrl+C)
-process.on('SIGINT', () => {
-  console.log("This important software is now closing");
-  process.exit();
+// Add an event handler for 'end' to stdin and display the close message
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
